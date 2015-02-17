0d16a38756798cabc9b511e86a0455339ecebae3
David N. Robinson <oaguy1@gmail.com>
2015-02-16 22:06:34 -0500

    WIP: Static text from sefeia.org, nav in place

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -36,9 +36,6 @@ angular.module('starter.controllers', [])
 .controller('PlaylistsCtrl', function($scope, $http) {
   $scope.date = new Date();
   var base_url = 'http://www.hebcal.com/converter/';
-  
-  console.log(JSON.stringify($scope.date.getYear()));
-  
   var url = base_url; 
   url += '?cfg=' + 'json';
   url += '&gy=' + $scope.date.getFullYear();
@@ -49,11 +46,50 @@ angular.module('starter.controllers', [])
   $http.get(url).then(function(resp) {
     $scope.success = true;
     $scope.resp = resp.data;
+
+    for(var i = 0; i < resp.data.events.length; i++) {
+      if(resp.data.events[i].substring(0, "Parashat".length) === "Parashat") {
+        $scope.parsha = resp.data.events[i];
+      }
+    }
   }, function(err) {
     $scope.success = false;
     $scope.resp = err;
   });
+
+  var base_url = 'http://www.sefaria.org/api/texts/Bereishit.25:1-27:19';
+  var url = base_url;
+  url += '?context=0';
+  url += '&commentary=0';
+  url += '&callback=JSON_CALLBACK'
+    $http.jsonp(url).then(function(resp) {
+      $scope.success = true;
+      var text = resp.data.text;
+      var en = [];
+      var chapter = 25;
+      $scope.book = resp.data.book;
+      for(var i = 0; i < text.length; i++){
+        en[i] = {
+          chapter : chapter,
+          text    : []
+        };
+        for(var j = 0; j < text[i].length; j++) {
+          en[i].text[j] = {
+            verse :  j + 1,
+            text  : text[i][j]
+          };
+        };
+        chapter++;
+      }
+      $scope.en = en;
+    }, function(err) {
+    $scope.success = false;
+    $scope.text = err;
+  });
+
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 });
+
+
